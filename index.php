d9020309dd50c9c5d2fca38bc18d95035b14089d
Debusschere Alexandre <zizilex@gmail.com>
2014-07-02 23:42:09 +0700

    First commit
    Start of the project based on a Web Matrix 3 PHP starter website
    template.
    A lot lot lot still need to be done.

---
+++ index.php
@@ -0,0 +1,21 @@
+    <?php 
+        require_once ("Includes/simplecms-config.php"); 
+        require_once  ("Includes/connectDB.php");
+        include("Includes/header.php");         
+     ?>
+
+<!-- 
+
+Load profile content and display here
+Configuration :
+    1. Contenu de la page d'acceuil
+    2. Site name
+    3. Formulaire en $_POST
+
+-->
+
+</div> <!-- .container from header.php-->
+
+<?php 
+    include ("Includes/footer.php");
+ ?>
